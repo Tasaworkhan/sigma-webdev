d21f7a6253cacd96bf224986b881894a50decf42
Tasawor120 <tasaworkhan.sce@gmail.com>
2023-12-27 19:14:36 +0500

    new files

---
+++ video 55/script.js
@@ -0,0 +1,28 @@
+let a =10
+let b =45
+
+console.log(a+b)
+
+{
+    let a=20
+    console.log(a)
+}
+
+console.log(a)
+
+let o={
+   name: "tasawor",
+   Salary: 5000, 
+}
+
+console.log(o)
+
+z= "tasawor"
+x= 45.5
+y= true
+k= undefined
+l= null
+
+console.log(z,x,y,k,l)
+
+console.log(typeof z , typeof x, typeof y, typeof k, typeof l)
